c977906b285a505da13cc67e87cc276e323e3aa9
elfrieddasilva <elfrieddasilva@gmail.com>
2025-01-21 01:20:52 +0100

    import lint fix

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
-import { useProjectService } from "@/context/DI";
+//import { useProjectService } from "@/context/DI";
 
 
 
